46c291657427d08a96094cf92675d6f81824a9ba
hydazz <alexanderhyde@icloud.com>
2026-08-25 17:08:55 +1000

    chore: xcode bootstrap

---
+++ Plinth/PlinthApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlinthApp.swift
+//  Plinth
+//
+//  Created by Alexander Hyde on 25/8/2026.
+//
+
+import SwiftUI
+
+@main
+struct PlinthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
